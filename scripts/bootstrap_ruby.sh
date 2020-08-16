d21ad8af01e5b8faefff0c6891314ca183831e7f
Patrik Majer <patrik.majer.pisek@gmail.com>
2020-08-16 19:15:44 +0200

    fix bootstrap ruby

--- scripts/bootstrap_ruby.sh
+++ scripts/bootstrap_ruby.sh
@@ -13,13 +13,16 @@ dnf module -y reset ruby
 dnf module -y enable ruby:2.6
 dnf module -y install ruby:2.6
 
-yum -q -y install gcc-c++ patch readline-devel zlib-devel libxml2-devel libyaml-devel libxslt-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison git
+yum -q -y install gcc gcc-c++ patch readline-devel zlib-devel libxml2-devel libyaml-devel libxslt-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison git
 
-yum install make ruby-devel redhat-rpm-config -y
+yum install make ruby-devel redhat-rpm-config -y -q
 
-dnf install --enablerepo=PowerTools augeas augeas-devel -y
+dnf install --enablerepo=PowerTools augeas augeas-devel -y -q
 
 #echo 'source /opt/rh/rh-ruby27/enable' >> /root/.bashrc
+
+echo 'export PATH="$PATH:/usr/local/bin"' >> /etc/profile.d/ruby-env.sh
+
 #
 #echo '' >> /root/.bashrc;
 #echo 'unset GEM_HOME' >> /root/.bashrc;
